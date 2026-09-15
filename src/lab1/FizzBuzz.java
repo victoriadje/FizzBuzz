f82b97fb03ede46949f8752685a0e0f5b7973956
Victoria Djedjeian <victoriadjedjeian2025@gmail.com>
2026-09-15 19:02:57 -0400

    Commit fizzbuzz while

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -1,5 +1,4 @@
 package lab1;
-
 /**
  * Solve the lab1.FizzBuzz challenge.
  */
@@ -7,8 +6,9 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 100; i++) {
-
+      //for (int i = 1; i <= 100; i++) {
+        int i = 1;
+        while(i<= 100){
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
             boolean divisibleBy5 = i % 5 == 0;
@@ -31,6 +31,7 @@ class FizzBuzz {
                 System.out.println(i);
 
             }
+            i++;;
         }
     }
 }
